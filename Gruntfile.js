01a4099d2af9f25ce32e3b75d1c1372bab7502b5
Nicolas Chalet <nicolas.chalet@gmail.com>
2015-10-30 06:34:43 +0100

    finishing build
    cleanup almost ok (remains yo stylesheet)

--- Gruntfile.js
+++ Gruntfile.js
@@ -209,7 +209,7 @@ module.exports = function (grunt) {
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
       options: {
-        sassDir: '<%= yeoman.app %>/styles',
+        sassDir: '<%= yeoman.app %>',
         cssDir: '.tmp/styles',
         generatedImagesDir: '.tmp/images/generated',
         imagesDir: '<%= yeoman.app %>/images',
