59d426a04c34e0cafab98f6da937fcfd755ddbff
Nicolas Chalet <nicolas.chalet@gmail.com>
2015-10-30 02:04:42 +0100

    here we come

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kspCoolToolsApp
+ * @description
+ * # kspCoolToolsApp
+ *
+ * Main module of the application.
+ */
+angular.module('kspCoolToolsApp', [
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
